0fa17cf5979c3b235ce14939fab5cb8090c648a0
natcamh <herreranataliacamila@gmail.com>
2023-12-16 18:18:12 -0300

    js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
 "use strict";
+// Buttons
+const addBtn = document.getElementById('addBtn');
+const checkBtn = document.querySelector('checkBtn');
+const arrowBtn = document.getElementById('arrowBtn');
+
+//Cards
+const cardToDo = document.getElementById('cardToDo');
+const cardInProgress = document.getElementById('cardInProgress');
+const cardDone = document.getElementById('cardDone');
+
+
+addBtn.addEventListener('click', ()=>{
+    createTask();
+})
+
+function createTask(){
+    const newTask = document.createElement('div');
+    newTask.innerHTML =`
+    <div class="task">
+        <img src="/assets/check.png" alt="check" id="checkBtn">
+        <p>Task</p>
+        <img src="/assets/arrow-pink.png" alt="in-progress" id="arrowBtn"> 
+    </div>
+    `;
+    cardToDo.appendChild(newTask);
+    moveTask(addBtn, cardDone, newTask);
+}
+
+function moveTask(btn, card, task){
+    btn.addEventListener('click', ()=>{
+        card.innerHTML = task;
+        card.appendChild(task);
+
+    });
+}
+
